31915d6822a4212bf276ecf45682c000ab19decf
durga2677 <125868369+durga2677@users.noreply.github.com>
2023-08-01 12:25:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
+  return decimal.toString(2);
+}
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
   
 }
 
